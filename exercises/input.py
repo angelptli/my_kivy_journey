e6cb1cf75861df5633119330b7bdcd0844d3f4e0
angelptli <ali95@mail.ccsf.edu>
2021-06-30 15:44:30 -0700

    Allow user input and return input in a sentence on screen

--- exercises/input.py
+++ exercises/input.py
@@ -7,7 +7,55 @@ from kivy.uix.button import Button
 
 
 class MyGridLayout(GridLayout):
+    # Initialize infinite keywords **kwargs
 
+    def __init__(self, **kwargs):
+        # Call grid layout constructor super
+        super(MyGridLayout, self).__init__(**kwargs)
+
+        # Set columns
+        self.cols = 1
+
+        # Add widgets below:
+        # Indicate widget's text
+        self.add_widget(Label(text="Name: "))
+        # Add input box next to the above widget and turn on multiline
+        self.name = TextInput(multiline=True)
+        # Finish adding the widget
+        self.add_widget(self.name)
+
+        self.add_widget(Label(text="Favorite Pizza: "))
+        self.pizza = TextInput(multiline=False)
+        self.add_widget(self.pizza)
+
+        self.add_widget(Label(text="Favorite Color: "))
+        self.color = TextInput(multiline=False)
+        self.add_widget(self.color)
+
+        # Create a submit button
+        self.submit = Button(text="Submit", font_size=32)
+        # Bind the button to make it clickable
+        self.submit.bind(on_press=self.press)
+        self.add_widget(self.submit)
+
+    def press(self, instance):
+        """Create variables of the input boxes.
+        """
+        name = self.name.text
+        pizza = self.pizza.text
+        color = self.color.text
+
+        # print(f'Hi {name}, you like {pizza} pizza, '
+        #       f'and your favorite color is {color}.')
+
+        # Print it on the app screen
+        self.add_widget(Label(text=f'Hi {name}, you like {pizza} pizza, '
+                                   f'and your favorite color is {color}.'))
+
+        # Clear the input boxes
+        self.name.text = ""
+        self.pizza.text = ""
+        self.color.text = ""
 
 
 class MyApp(App):
@@ -18,13 +66,10 @@ class MyApp(App):
     """
 
     def build(self):
-        """Initialize a Label that will be the Root Widget of this app.
-
-        :return: Label with the indicated text
-        :rtype: MyApp object
+        """Display app with the MyGridLayout widgets.
         """
         # Return a label
-        return Label(text="Hello World")
+        return MyGridLayout()
 
 
 if __name__ == '__main__':
