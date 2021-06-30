856fae36325bace357375fd0df7bbbb93a393793
angelptli <ali95@mail.ccsf.edu>
2021-06-30 13:54:52 -0700

    Initial commit

---
+++ exercises_programs/hello.py
@@ -0,0 +1,14 @@
+import kivy
+from kivy.app import App
+from kivy.uix.label import Label
+
+
+class MyApp(App):
+    def build(self):
+        # Return a label
+        return Label(text="Hello World")
+
+
+if __name__ == '__main__':
+    # Run MyApp
+    MyApp().run()
