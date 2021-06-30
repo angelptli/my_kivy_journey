4538082b169c52dfcff8f6643a08764727214b01
angelptli <ali95@mail.ccsf.edu>
2021-06-30 14:46:27 -0700

    Add docstrings to hello.py and input.py

--- exercises/input.py
+++ exercises/input.py
@@ -1,6 +1,13 @@
 import kivy
 from kivy.app import App
 from kivy.uix.label import Label
+from kivy.uix.gridlayout import GridLayout
+from kivy.uix.textinput import TextInput
+from kivy.uix.button import Button
+
+
+class MyGridLayout(GridLayout):
+
 
 
 class MyApp(App):
