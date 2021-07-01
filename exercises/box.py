2c1f0774d504ff84c74708a3d8eb28c07fcce59e
angelptli <ali95@mail.ccsf.edu>
2021-06-30 19:37:43 -0700

    Customize size, spacing, and padding of buttons.

---
+++ exercises/box.py
@@ -0,0 +1,34 @@
+# File: box.py
+# Desc: Accomodate python code for box customization exercise.
+
+import kivy
+from kivy.app import App
+from kivy.uix.widget import Widget
+from kivy.properties import ObjectProperty
+from kivy.lang import Builder
+
+# Load Kv file with whatever path or filename. This looks only for whatever.kv.
+Builder.load_file('box.kv')
+
+
+class MyLayout(Widget):
+    pass
+
+
+class NiceApp(App):
+    """Display Hello World in the app.
+
+    :param App: Contains the build() method
+    :type App: class
+    """
+
+    def build(self):
+        """Display app with the MyLayout widgets.
+        """
+        # Return a label
+        return MyLayout()
+
+
+if __name__ == '__main__':
+    # Run MyApp
+    NiceApp().run()
